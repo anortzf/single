46a2e9d8e17c0101045ac42b57ea990165859e7b
anortzf <54248745+anortzf@users.noreply.github.com>
2021-10-01 16:32:10 -0300

    Update and rename file.js to file8.js

--- file8.js
+++ file8.js
@@ -12,6 +12,7 @@
         "name_episodio": "episodio 1",
         "link": "/Drive the movie"
       },
+      
       {
         "img1": "http://pic0.iqiyipic.com/image/20210816/05/1d/v_161484497_m_601_zh-TW_m2_220_124.jpg",
         "alt": "la dama del agua",
@@ -19,6 +20,7 @@
         "name_episodio": "episodio 1",
         "link": "/Drive the movie"
       },
+      
       {
         "img1": "http://pic0.iqiyipic.com/image/20210816/05/1d/v_161484497_m_601_zh-TW_m2_220_124.jpg",
         "alt": "la dama del agua",
@@ -26,6 +28,7 @@
         "name_episodio": "episodio 1",
         "link": "/Drive the movie"
       },
+      
      {
         "img1": "http://pic0.iqiyipic.com/image/20210816/05/1d/v_161484497_m_601_zh-TW_m2_220_124.jpg",
         "alt": "la dama del agua",
@@ -33,6 +36,7 @@
         "name_episodio": "episodio 1",
         "link": "/Drive the movie"
       },
+      
      {
         "img1": "http://pic0.iqiyipic.com/image/20210816/05/1d/v_161484497_m_601_zh-TW_m2_220_124.jpg",
         "alt": "la dama del agua",
@@ -40,6 +44,7 @@
         "name_episodio": "episodio 1",
         "link": "/Drive the movie"
       },
+      
      {
         "img1": "http://pic0.iqiyipic.com/image/20210816/05/1d/v_161484497_m_601_zh-TW_m2_220_124.jpg",
         "alt": "la dama del agua",
@@ -54,6 +59,10 @@
         "title": "Drive the movie",
         "link_s": "/Drive the movie"
       },
+      
+      
+      
+      
      {
         "img1": "http://pic0.iqiyipic.com/image/20210816/05/1d/v_161484497_m_601_zh-TW_m2_220_124.jpg",
         "alt": "la dama del agua",
@@ -82,6 +91,7 @@
         "name_episodio": "episodio 1",
         "link": "/Drive the movie"
       }
+      
     ],
     "title": "Spiderman",
     "category": "PG18",
@@ -104,6 +114,7 @@
         "title": "Drive the movie",
         "link_s": "/Drive the movie"
       },
+      
       {
         "img1": "https://static.togglestatic.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%272893151%27&EntityType=%27Item%27&EntityId=%27206478%27&Width=336&Height=189&ImageUrl=%272893151.jpg%27&device=%27web_browser%27&subscriptions=%27Anonymous%27&segmentationTags=%27all%27&ResizeAction=%27fill%27&HorizontalAlignment=%27center%27&VerticalAlignment=%27top%27",
         "img2": "https://static.togglestatic.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%272893151%27&EntityType=%27Item%27&EntityId=%27206478%27&Width=336&Height=189&ImageUrl=%272893151.jpg%27&device=%27web_browser%27&subscriptions=%27Anonymous%27&segmentationTags=%27all%27&ResizeAction=%27fill%27&HorizontalAlignment=%27center%27&VerticalAlignment=%27top%27",
@@ -118,6 +129,7 @@
         "title": "Drive the movie",
         "link_s": "/Drive the movie"
       },
+      
       {
         "img1": "https://static.togglestatic.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%272893151%27&EntityType=%27Item%27&EntityId=%27206478%27&Width=336&Height=189&ImageUrl=%272893151.jpg%27&device=%27web_browser%27&subscriptions=%27Anonymous%27&segmentationTags=%27all%27&ResizeAction=%27fill%27&HorizontalAlignment=%27center%27&VerticalAlignment=%27top%27",
         "img2": "https://static.togglestatic.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%272893151%27&EntityType=%27Item%27&EntityId=%27206478%27&Width=336&Height=189&ImageUrl=%272893151.jpg%27&device=%27web_browser%27&subscriptions=%27Anonymous%27&segmentationTags=%27all%27&ResizeAction=%27fill%27&HorizontalAlignment=%27center%27&VerticalAlignment=%27top%27",
@@ -125,6 +137,7 @@
         "title": "Drive the movie",
         "link_s": "/Drive the movie"
       },
+      
       {
         "img1": "https://static.togglestatic.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%272893151%27&EntityType=%27Item%27&EntityId=%27206478%27&Width=336&Height=189&ImageUrl=%272893151.jpg%27&device=%27web_browser%27&subscriptions=%27Anonymous%27&segmentationTags=%27all%27&ResizeAction=%27fill%27&HorizontalAlignment=%27center%27&VerticalAlignment=%27top%27",
         "img2": "https://static.togglestatic.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%272893151%27&EntityType=%27Item%27&EntityId=%27206478%27&Width=336&Height=189&ImageUrl=%272893151.jpg%27&device=%27web_browser%27&subscriptions=%27Anonymous%27&segmentationTags=%27all%27&ResizeAction=%27fill%27&HorizontalAlignment=%27center%27&VerticalAlignment=%27top%27",
@@ -132,6 +145,7 @@
         "title": "Drive the movie",
         "link_s": "/Drive the movie"
       },
+      
       {
         "img1": "https://static.togglestatic.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%272893151%27&EntityType=%27Item%27&EntityId=%27206478%27&Width=336&Height=189&ImageUrl=%272893151.jpg%27&device=%27web_browser%27&subscriptions=%27Anonymous%27&segmentationTags=%27all%27&ResizeAction=%27fill%27&HorizontalAlignment=%27center%27&VerticalAlignment=%27top%27",
         "img2": "https://static.togglestatic.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%272893151%27&EntityType=%27Item%27&EntityId=%27206478%27&Width=336&Height=189&ImageUrl=%272893151.jpg%27&device=%27web_browser%27&subscriptions=%27Anonymous%27&segmentationTags=%27all%27&ResizeAction=%27fill%27&HorizontalAlignment=%27center%27&VerticalAlignment=%27top%27",
@@ -139,6 +153,7 @@
         "title": "Drive the movie",
         "link_s": "/Drive the movie"
       },
+      
       {
         "img1": "https://static.togglestatic.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%272893151%27&EntityType=%27Item%27&EntityId=%27206478%27&Width=336&Height=189&ImageUrl=%272893151.jpg%27&device=%27web_browser%27&subscriptions=%27Anonymous%27&segmentationTags=%27all%27&ResizeAction=%27fill%27&HorizontalAlignment=%27center%27&VerticalAlignment=%27top%27",
         "img2": "https://static.togglestatic.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%272893151%27&EntityType=%27Item%27&EntityId=%27206478%27&Width=336&Height=189&ImageUrl=%272893151.jpg%27&device=%27web_browser%27&subscriptions=%27Anonymous%27&segmentationTags=%27all%27&ResizeAction=%27fill%27&HorizontalAlignment=%27center%27&VerticalAlignment=%27top%27",
@@ -146,6 +161,7 @@
         "title": "Drive the movie",
         "link_s": "/Drive the movie"
       },
+      
       {
         "img1": "https://static.togglestatic.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%272893151%27&EntityType=%27Item%27&EntityId=%27206478%27&Width=336&Height=189&ImageUrl=%272893151.jpg%27&device=%27web_browser%27&subscriptions=%27Anonymous%27&segmentationTags=%27all%27&ResizeAction=%27fill%27&HorizontalAlignment=%27center%27&VerticalAlignment=%27top%27",
         "img2": "https://static.togglestatic.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%272893151%27&EntityType=%27Item%27&EntityId=%27206478%27&Width=336&Height=189&ImageUrl=%272893151.jpg%27&device=%27web_browser%27&subscriptions=%27Anonymous%27&segmentationTags=%27all%27&ResizeAction=%27fill%27&HorizontalAlignment=%27center%27&VerticalAlignment=%27top%27",
@@ -153,6 +169,7 @@
         "title": "Drive the movie",
         "link_s": "/Drive the movie"
       },
+      
       {
         "img1": "https://static.togglestatic.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%272893151%27&EntityType=%27Item%27&EntityId=%27206478%27&Width=336&Height=189&ImageUrl=%272893151.jpg%27&device=%27web_browser%27&subscriptions=%27Anonymous%27&segmentationTags=%27all%27&ResizeAction=%27fill%27&HorizontalAlignment=%27center%27&VerticalAlignment=%27top%27",
         "img2": "https://static.togglestatic.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%272893151%27&EntityType=%27Item%27&EntityId=%27206478%27&Width=336&Height=189&ImageUrl=%272893151.jpg%27&device=%27web_browser%27&subscriptions=%27Anonymous%27&segmentationTags=%27all%27&ResizeAction=%27fill%27&HorizontalAlignment=%27center%27&VerticalAlignment=%27top%27",
@@ -160,6 +177,7 @@
         "title": "Drive the movie",
         "link_s": "/Drive the movie"
       },
+      
       {
         "img1": "https://static.togglestatic.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%272893151%27&EntityType=%27Item%27&EntityId=%27206478%27&Width=336&Height=189&ImageUrl=%272893151.jpg%27&device=%27web_browser%27&subscriptions=%27Anonymous%27&segmentationTags=%27all%27&ResizeAction=%27fill%27&HorizontalAlignment=%27center%27&VerticalAlignment=%27top%27",
         "img2": "https://static.togglestatic.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%272893151%27&EntityType=%27Item%27&EntityId=%27206478%27&Width=336&Height=189&ImageUrl=%272893151.jpg%27&device=%27web_browser%27&subscriptions=%27Anonymous%27&segmentationTags=%27all%27&ResizeAction=%27fill%27&HorizontalAlignment=%27center%27&VerticalAlignment=%27top%27",
