01e845d012900b3617ef227476b8e8ecb7313b62
anortzf <54248745+anortzf@users.noreply.github.com>
2021-10-01 16:39:40 -0300

    Update and rename single2.js to single3.js

--- single3.js
+++ single3.js
@@ -37,17 +37,7 @@
         "name_episodio": "episodio 1",
         "link": "/Drive the movie"
       },
-      {
-        "img1": "https://static.togglestatic.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%272893151%27&EntityType=%27Item%27&EntityId=%27206478%27&Width=336&Height=189&ImageUrl=%272893151.jpg%27&device=%27web_browser%27&subscriptions=%27Anonymous%27&segmentationTags=%27all%27&ResizeAction=%27fill%27&HorizontalAlignment=%27center%27&VerticalAlignment=%27top%27",
-        "img2": "https://static.togglestatic.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%272893151%27&EntityType=%27Item%27&EntityId=%27206478%27&Width=336&Height=189&ImageUrl=%272893151.jpg%27&device=%27web_browser%27&subscriptions=%27Anonymous%27&segmentationTags=%27all%27&ResizeAction=%27fill%27&HorizontalAlignment=%27center%27&VerticalAlignment=%27top%27",
-        "img3": "https://static.togglestatic.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=45&ImageId=%272893151%27&EntityType=%27Item%27&EntityId=%27206478%27&Width=448&Height=252&ImageUrl=%272893151.jpg%27&device=%27web_browser%27&subscriptions=%27Anonymous%27&segmentationTags=%27all%27&ResizeAction=%27fill%27&HorizontalAlignment=%27center%27&VerticalAlignment=%27top%27",
-        "title": "Drive the movie",
-        "link_s": "/Drive the movie"
-      },
-      
-      
-      
-      
+
      {
         "img1": "http://pic0.iqiyipic.com/image/20210816/05/1d/v_161484497_m_601_zh-TW_m2_220_124.jpg",
         "alt": "la dama del agua",
@@ -55,6 +45,35 @@
         "name_episodio": "episodio 1",
         "link": "/Drive the movie"
       },
+       {
+        "img1": "http://pic0.iqiyipic.com/image/20210816/05/1d/v_161484497_m_601_zh-TW_m2_220_124.jpg",
+        "alt": "la dama del agua",
+        "data_link": "/link del iframe",
+        "name_episodio": "episodio 1",
+        "link": "/Drive the movie"
+      },
+       {
+        "img1": "http://pic0.iqiyipic.com/image/20210816/05/1d/v_161484497_m_601_zh-TW_m2_220_124.jpg",
+        "alt": "la dama del agua",
+        "data_link": "/link del iframe",
+        "name_episodio": "episodio 1",
+        "link": "/Drive the movie"
+      },
+       {
+        "img1": "http://pic0.iqiyipic.com/image/20210816/05/1d/v_161484497_m_601_zh-TW_m2_220_124.jpg",
+        "alt": "la dama del agua",
+        "data_link": "/link del iframe",
+        "name_episodio": "episodio 1",
+        "link": "/Drive the movie"
+      },
+       {
+        "img1": "http://pic0.iqiyipic.com/image/20210816/05/1d/v_161484497_m_601_zh-TW_m2_220_124.jpg",
+        "alt": "la dama del agua",
+        "data_link": "/link del iframe",
+        "name_episodio": "episodio 1",
+        "link": "/Drive the movie"
+      },
+      
       {
         "img1": "http://pic0.iqiyipic.com/image/20210816/05/1d/v_161484497_m_601_zh-TW_m2_220_124.jpg",
         "alt": "la dama del agua",
